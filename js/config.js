1d514733e45a11613bd7d7dcb6a0b235fa6c3631
Vicentbe <35101402+Vicentbe@users.noreply.github.com>
2024-01-17 00:10:40 +0900

    cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_IdkdqFUez', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1cq98pdnkoipbcu92kh1tl5fru', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
